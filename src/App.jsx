e8a5f20c1e94e26bfb300eabe315cb7c6266b296
Erick Chavez <erickchavezescobar@gmail.com>
2024-03-31 01:02:11 -0600

    parallex added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,51 @@ import NavBar from './components/NavBar'
 // import FullPageScroll from './pages/FullPageScroll'
 import { Parallax } from 'react-parallax'
 import woods from './img/woods.jpeg'
-import fog from './img/fog.jpg'
+import me from './img/me_comp.png'
+// import p0 from './img/p0.webp'
 
 function App () {
   return (
     <>
       <NavBar />
       {/* <FullPageScroll /> */}
-      <Parallax strength={600} bgImage={woods}>
-        <div className='hero--content'>
-          <div className='hero--content_text'>
+      {/* Parallax tutorial: https://www.youtube.com/watch?v=XUmq3B7Iugw */}
+      <Parallax strength={-600} bgImage={woods}>
+        <div className='content'>
+          <div className='content_text'>
             <p className='welcome'>WELCOME</p>
             <p className='message'>Thanks for your visit</p>
           </div>
+        </div>
+      </Parallax>
+      <Parallax>
+        <div className='about'>
+          <div className='about_text'>
+            <h1 className='about-header'>About me</h1>
+            <p>I am interested in timeless designs that capture the essence of emotions, bring joy to those who see it and inevitably create an experience.</p>
+            <p>I am currently taking a UX Design course at Google and I am countinously learning about front end technologies since I enjoy bringing to life anything that I can picture in my mind.</p>
+            <p>The technologies I am familiar with are: HTML, CSS, JS, Typescript, Bootstrap, GitHub, React, Figma & Adobe Illustrator.</p>
+          </div>
+          <img src={me} alt='profile photo of Erick' className='me' />
+        </div>
+      </Parallax>
+      <Parallax>
+        <div className='projects'>
+          <div className='projects_text'>
+            <p className='projects-welcome'>PROJECTS</p>
+          </div>
+        </div>
+      </Parallax>
+      <Parallax>
+        <div className='contact'>
+          <div className='contact_text'>
+            <h1 className='about-header'>Contact</h1>
+            <p>LinkedIn</p>
+            <p>Mail</p>
+            <p>Phone</p>
+            <p>WhatsApp</p>
+          </div>
+
         </div>
       </Parallax>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,30 @@
 import logo from '../assets/logo_3.png'
+import '../pages/pages.css'
 
 const NavBar = () => {
   return (
 
-    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-      <div className='container-fluid d-flex'>
+    <nav className='navbar navbar-expand-lg bg-body-tertiary position-fixed'>
+      <div className='container-fluid'>
         <a className='navbar-brand' href='#'>
           <img src={logo} alt='Logo' height='24' className='d-inline-block align-text-top  mx-2' />
           Erick Chávez Escobar
         </a>
-        <div className='buttons'>
-          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon' />
-          </button>
-          <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav'>
-              <li className='nav-item'>
-                <a className='nav-link active' aria-current='page' href='#'>About me</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#'>Projects</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='#'>Contact</a>
-              </li>
-            </ul>
-          </div>
+        <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNav'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
+              <a className='nav-link active' aria-current='page' href='#'>About me</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Projects</a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>Contact</a>
+            </li>
+          </ul>
         </div>
 
       </div>
