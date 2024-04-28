cd8f41e56380e701e3c0620a1e825d98370cae1f
Erick Chavez <erickchavezescobar@gmail.com>
2024-04-27 23:18:25 -0600

    instagram icon replaced by github icon in Contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import './pages.css'
 import mail from '../img/envelope.jpg'
 import linked from '../img/linkedin.png'
 import gmail from '../img/gmail.png'
-import instagram from '../img/instagram.png'
+import github from '../img/github.png'
 import whatsapp from '../img/whatsapp.png'
 
 const Contact = () => {
@@ -21,14 +21,14 @@ const Contact = () => {
                 <img className='network' src={linked} alt='' style={{ height: '40px' }} />
               </span>
             </div>
+            <div className='git_box box'>
+              <span>
+                <img className='network' src={github} alt='' style={{ height: '40px' }} />
+              </span>
+            </div>
             <div className='mail_box box'>
               <span>
                 <img className='network' src={gmail} alt='' style={{ height: '40px' }} />
-              </span>
-            </div>
-            <div className='phone_box box'>
-              <span>
-                <img className='network' src={instagram} alt='' style={{ height: '40px' }} />
               </span>
             </div>
             <div className='whats_box box'>
